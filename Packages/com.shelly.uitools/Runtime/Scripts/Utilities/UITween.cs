702ec1c87ac01b4b768383332bf11ff9ee1c436b
YiHsuan <shellypooh819@gmail.com>
2026-06-18 14:29:26 +0800

    :sparkles: add type write and button press effect

--- Packages/com.shelly.uitools/Runtime/Scripts/Utilities/UITween.cs
+++ Packages/com.shelly.uitools/Runtime/Scripts/Utilities/UITween.cs
@@ -1,4 +1,6 @@
-﻿using PrimeTween;
+﻿using System;
+using PrimeTween;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -23,6 +25,22 @@ namespace Shelly.UITools
 
         public static Tween FadeOut(this Graphic graphic, float duration = 0.3f, Ease ease = Ease.Linear)
             => Tween.Alpha(graphic, endValue: 0f, duration, ease);
+
+        public static Tween TypeWrite(this TextMeshProUGUI text, string content, float duration = 1f, Action onComplete = null)
+        {
+            text.maxVisibleCharacters = 0;
+            text.text                 = content;
+            text.ForceMeshUpdate();
+
+            return Tween.Custom(0, content.Length, duration,
+                            val => text.maxVisibleCharacters = (int)val, Ease.Linear)
+                        .OnComplete(() => onComplete?.Invoke());
+        }
+
+        public static Sequence ButtonPress(this Transform t,
+            float pressScale = 0.8f, float pressDuration = 0.05f, float releaseDuration = 0.05f, Ease ease = Ease.Linear)
+            => Sequence.Create()
+                       .Chain(Tween.Scale(t, pressScale, pressDuration, ease))
+                       .Chain(Tween.Scale(t, 1f, releaseDuration, ease));
     }
 }
-
